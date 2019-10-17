dc82907bcfe50b882ba7e6337f630258f21d3ba1
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-10-17 10:02:56 -0400

    writing command

---
+++ src/com/LickinHeights/Main.java
@@ -0,0 +1,21 @@
+package com.LickinHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("hello world");
+       System.out.println("Hello star");
+       System.out.println("hello university");
+       System.out.println("moon");
+
+
+
+       
+    }
+
+
+
+
+
+
+}
